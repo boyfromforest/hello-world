150995f0259dddea6668bdd143703b40f8afeff1
Aforvard <62892137+Aforvard@users.noreply.github.com>
2020-03-30 18:34:04 +0300

    Create Class.cpp

---
+++ Class.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+const double Pi = 3.14;
+class Sphere 
+{
+private: 
+	int r;
+public: 
+	Sphere(int radius ) {
+		setRadius(radius); 
+	}
+	void setRadius(int radius ){
+		if (radius > 0)	{
+			r = radius;
+		}
+		else{
+			r = 1;
+		}
+	}
+    int getRadius()	{
+		return r;
+	}
+	int getDiameter() {
+		return 2 * r;
+	}
+	double getLKola() {
+		return Pi * getDiameter();
+	}
+	double getArea() {
+		return 4 * Pi*r*r;
+	}
+	double getVolume() {
+		int r3 = r * r*r;
+		return 4 * Pi*r3 / 3;
+	}
+
+}; 
+
+int main()
+{
+	int inpt;
+	cout << "Enter radius ";
+	cin >> inpt;
+	Sphere sph(inpt);
+	cout <<"Radius = "<<sph.getRadius() << endl;
+	cout <<"Diameter = "<<sph.getDiameter() << endl;
+	cout <<"LKola = "<<sph.getLKola() << endl;
+	cout <<"Area = " << sph.getArea() << endl;
+	cout <<"Volume = " << sph.getVolume() << endl;
+
+	return 0;
+}
